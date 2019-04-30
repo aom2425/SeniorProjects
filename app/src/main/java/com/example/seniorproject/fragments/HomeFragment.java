f1370ef548e20aca27137dc2a8d7e38192d4b34a
Grantas <dapkus@yahoo.com>
2019-04-30 01:19:34 -0400

    Added Separate Registration, and mains screens

--- app/src/main/java/com/example/seniorproject/fragments/HomeFragment.java
+++ app/src/main/java/com/example/seniorproject/fragments/HomeFragment.java
@@ -3,13 +3,25 @@ package com.example.seniorproject.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.RequestQueue;
+import com.example.seniorproject.ExampleAdapter;
+import com.example.seniorproject.ExampleItem;
 import com.example.seniorproject.R;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mExampleAdapter;
+    private ArrayList<ExampleItem> mExampleList;
+    private RequestQueue mRequestQueue;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
